6b5a11b0624be843020e4ccc680abd7e64dfbadf
Eliza <eliza.kimori@kimori.org>
2021-08-14 08:30:56 -0400

    makethumbs.sh

--- maps/makethumbs.sh
+++ maps/makethumbs.sh
@@ -9,14 +9,13 @@ for x in *.png; do
      len="${#x}"
      if [ $len -gt $suffixl ]; then
    			 fsuffix="${x: $dist}"
-   			 if [ "$fsuffix"  != "$suffix" ]; then
-				 		printf '%s\n' "Processing $x"
-   						convert -thumbnail x${thumbwidth} "$x" "$x$suffix";
-   			 fi
-
-
+		 else
+			 fsuffix=""
      fi
-
+     if [ "$fsuffix"  != "$suffix" ]; then
+	printf '%s\n' "Processing $x"
+   	convert -thumbnail x${thumbwidth} "$x" "$x$suffix";
+    fi
 done
 
 
